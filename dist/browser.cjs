afd07e44d23b86691913e0862928eb17d8795c60
Pratik Bodawala <pratikb@york.ie>
2024-10-21 21:57:19 +0530

    :hankey: temporary pushing dist

---
+++ dist/browser.cjs
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const browser = require('./shared/ollama.a893dbf5.cjs');
+require('node-fetch');
+
+
+
+exports.Ollama = browser.Ollama;
+exports.default = browser.browser;
---
+++ dist/browser.mjs
@@ -0,0 +1,2 @@
+export { O as Ollama, b as default } from './shared/ollama.44cb24a8.mjs';
+import 'node-fetch';
